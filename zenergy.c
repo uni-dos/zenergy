12c20496cf97c89faffa728af94b8a240e6f09c2
uni-dos <uni.dos12@outlook.com>
2025-03-02 12:21:49 -0800

    add zen5 and phoenix desktop

--- zenergy.c
+++ zenergy.c
@@ -400,6 +400,8 @@ static const struct x86_cpu_id cpu_ids[] __initconst = {
 	X86_MATCH_VENDOR_FAM_MODEL(AMD, 0x19, 0x60, NULL),	/* Rembrandt */
 	X86_MATCH_VENDOR_FAM_MODEL(AMD, 0x19, 0x61, NULL),	/* Zen 4 */
 	X86_MATCH_VENDOR_FAM_MODEL(AMD, 0x19, 0x74, NULL),	/* Phoenix */
+	X86_MATCH_VENDOR_FAM_MODEL(AMD, 0x19, 0x75, NULL),	/* Phoenix */
+	X86_MATCH_VENDOR_FAM_MODEL(AMD, 0x1A, 0x44, NULL),	/* Zen 5 */
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, cpu_ids);
